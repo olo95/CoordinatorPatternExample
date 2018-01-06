c04e1d070163c0872f0d6eb358ef4f6fe2a92685
olo95 <stolar95@onet.pl>
2018-01-06 21:23:41 +0100

    Created initial coordinator with initial view controller

---
+++ CoordinatorPatternExample/CoordinatorPatternExample/ViewControllers/Initial/InitialViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  InitialViewModel.swift
+//  CoordinatorPatternExample
+//
+//  Created by Alexander Stolar on 06.01.2018.
+//  Copyright © 2018 Alexander Stolar. All rights reserved.
+//
+
+import Foundation
